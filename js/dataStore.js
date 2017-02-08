ace5b8014a0e42834d09e12e52f37229a57c68fc
Andy <aliedholm@gmail.com>
2017-02-08 15:40:13 -0800

    fixed pushing of data arrays to sensors array

---
+++ js/dataStore.js
@@ -0,0 +1,23 @@
+//main data fetching and storing function
+function dataStore(sensor1, sensor2, sensor3, sensor4){
+	//ph1 data
+	d3.json("php/" + sensor1 + ".php", function(sens1points){
+		sens1 = sens1points;
+		sensors.push(sens1);
+	})
+	//ph2 data
+	d3.json("php/" + sensor2 + ".php", function(sens2points){
+		sens2 = sens2points;
+		sensors.push(sens2);
+	})
+	//ph3 data
+	d3.json("php/" + sensor3 + ".php", function(sens3points){
+		sens3 = sens3points;
+		sensors.push(sens3);
+	})
+	//ph4 data
+	d3.json("php/" + sensor4 + ".php", function(sens4points){
+		sens4 = sens4points;
+		sensors.push(sens4);
+	})
+}
