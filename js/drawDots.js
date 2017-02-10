a1a4e9217b11a435882919bf2c0a3719ad0e4385
Andy <aliedholm@gmail.com>
2017-02-10 14:46:42 -0800

    the page is working in a manually fashion with buttons to trigger events. Events are graphing, but the x axis range is way off and all the graph elements are starting over 1000pixels off

---
+++ js/drawDots.js
@@ -0,0 +1,18 @@
+//draw dots function
+function drawDots(dataObject, sensorTitles){
+	for (var i = 0; i < sensorTitles.length; i++){
+		canvas.selectAll("circle")
+			.data(dataObject[0][0])
+			.enter()
+			.append("circle")
+			.attr("class", (sensorTitles[i]) + "dots")
+			.style("fill", sensColors[i])
+			.attr("cx", function(d) {
+				return(xScale(timeParser(d.time)));
+			})
+			.attr("cy", function(d) {
+				return(yScale(d.reading));
+			})
+			.attr("r", circleSize)
+	}
+}			
---
+++ js/drawLine.js
@@ -0,0 +1,22 @@
+//defining linegen function
+var lineGen = d3.svg.line()
+	.x(function(d) {
+		return xScale(timeParser(d.time));
+	})
+	.y(function(d) {
+		return yScale(d.reading);
+	});
+
+//draw line function
+function drawLine(dataObject, sensorTitles){
+	for (var i = 0; i < sensorTitles.length; i++){
+		canvas.append('svg:path')
+			.attr('d', lineGen(dataObject[0][0]))
+			.attr("class", sensorTitles[i])
+			.attr("id", sensorTitles[i])
+			.attr('stroke', sensColors[i])
+			.attr('stroke-width', lineStroke)
+			.attr('fill', sensColors[i]);
+	}
+}
+
